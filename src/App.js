96eb8b3cc552c0ff65f860ccbcd3073ece802a10
Akın Can Aksoy <49723775+akncan07@users.noreply.github.com>
2022-11-06 14:24:04 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
+import logoTarge from './logo-targe.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logoTarge} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Powered by Targe Development
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.targe.com.tr"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Targe is loading..
         </a>
       </header>
     </div>
